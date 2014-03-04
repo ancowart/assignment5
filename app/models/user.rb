9cde1dd843562cc6f7d7d001695b6804cca83ff0
ancowart <ancowart@iupui.edu>
2014-03-03 19:09:37 -0500

    added associations and acts_as_authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  acts_as_authentic
+  belongs_to :lease
+  has_many :submittedreports, :class_name => 'RepairRequest'
+  has_many :approvedreports, :class_name => 'RepairRequest'
 end
